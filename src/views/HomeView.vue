448f8a867704dccfed20d35112d91a62886003a3
Jeremy Garey <jeremy@mirusresearch.com>
2024-11-11 00:53:04 -0600

    new landing page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,238 +1,126 @@
 <template>
-    <div>
-        <div class="sm:hidden">
-            <div
-                class="h-12 w-full absolute top-0 bg-black flex items-center z-40 lg:hidden"
-            >
-                <div class="container px-4 mx-auto flex justify-between">
-                    <a href="https://boxedwaterisbetter.com/" target="_blank">
-                        <box-logo class="fill-zinc-300 w-6 h-6" />
-                    </a>
-                    <div class="gap-4 hidden sm:flex">
-                        <a class="font-light" href="#">Hey</a>
-                        <a class="font-light" href="#">I'm</a>
-                        <a class="font-light" href="#">Jeremy</a>
-                        <a class="font-light" href="#">And</a>
-                        <a class="font-light" href="#">This</a>
-                        <a class="font-light" href="#">Is</a>
-                        <a class="font-light" href="#">My</a>
-                        <a class="font-light" href="#">Website</a>
-                    </div>
-                    <div class="flex gap-6 text-zinc-300">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            class="w-6 h-6"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-                            />
-                        </svg>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            class="w-6 h-6"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-                            />
-                        </svg>
-                        <button @click="isOpen = !isOpen">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                class="w-6 h-6"
-                                v-show="!isOpen"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M3.75 9h16.5m-16.5 6.75h16.5"
-                                />
-                            </svg>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                class="w-6 h-6"
-                                v-show="isOpen"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M6 18 18 6M6 6l12 12"
-                                />
-                            </svg>
-
-                            <!-- TODO: add menu here -->
-                            <!-- <div
-                                class="absolute top-0 left-0 right-0 z-100 bg-black text-zinc-300"
-                            >
-                                <div>hey there</div>
-                            </div> -->
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div
-                class="h-[100svh] flex flex-col justify-between items-center bg-cover bg-top pt-12"
-                style="background-image: url('/img/vision-pro-profile.webp')"
-            >
-                <div class="sticky top-0 left-0 w-full z-50 text-white">
-                    <div
-                        class="h-12 w-full bg-black/30 backdrop-blur-lg flex justify-between items-center px-4"
-                    >
-                        <div class="text-xl font-medium">Jeremy Garey</div>
-                        <div class="flex gap-3">
-                            <button @click="isMenu2Open = !isMenu2Open">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="1.5"
-                                    stroke="currentColor"
-                                    class="w-6 h-6"
-                                    v-show="!isMenu2Open"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        d="m19.5 8.25-7.5 7.5-7.5-7.5"
-                                    />
-                                </svg>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="1.5"
-                                    stroke="currentColor"
-                                    class="w-6 h-6"
-                                    v-show="isMenu2Open"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        d="m4.5 15.75 7.5-7.5 7.5 7.5"
-                                    />
-                                </svg>
-                            </button>
-                            <a
-                                href="https://www.instagram.com/jeremy.garey/"
-                                target="_blank"
-                                class="rounded-full bg-blue-500 text-xs px-3 py-1"
-                                >Say hi</a
-                            >
-                        </div>
-                    </div>
-                    <Transition name="slide-fade">
-                        <div
-                            class="absolute top-14 right-4 text-zinc-300"
-                            v-show="isMenu2Open"
-                        >
-                            <div
-                                class="bg-black/30 backdrop-blur-lg p-4 rounded-lg"
-                            >
-                                <ul class="dropdown-menu">
-                                    <li class="font-bold">
-                                        Some things I love:
-                                    </li>
-                                    <li>
-                                        <a
-                                            href="https://www.bible.com/bible/97/1JN.4.9.MSG"
-                                            target="_blank"
-                                            >Jesus</a
-                                        >
-                                    </li>
-                                    <li>
-                                        <a
-                                            href="https://www.youtube.com/watch?v=Iocivkb9Qeo"
-                                            target="_blank"
-                                            >Drums</a
-                                        >
-                                    </li>
-                                    <li>
-                                        <a
-                                            href="https://vsco.co/jeremygarey/media/62b91961fd64e2655d62d344"
-                                            target="_blank"
-                                            >Wakeboarding</a
-                                        >
-                                    </li>
-                                    <li>
-                                        <a
-                                            href="https://hackertyper.com/"
-                                            target="_blank"
-                                            >Web Dev</a
-                                        >
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </Transition>
-                </div>
-                <div class="text-center pb-12">
-                    <div class="font-bold text-4xl text-black">Oh hey</div>
-                    <p class="text-blue-600 mt-2 font-medium">
-                        Hang tight. Website coming soon.
-                    </p>
-                </div>
-            </div>
-            <div
-                class="h-40 text-white flex justify-center items-center container px-4"
-            >
-                <div class="text-center">
-                    <p class="text-lg font-semibold">thanks for visiting 👋🏻</p>
-                    <p class="text-xs mt-1"></p>
-                    <p class="text-xs mt-1">
-                        no, i don't actually have a pair of $4000 tech goggles.
-                        yes, i think this page is funnier than it actually is.
-                        and yes, you should totally go check out
-                        <a
-                            class="text-blue-400 font-semibold"
-                            href="https://vsco.co/jeremygarey/gallery"
-                            >my vsco</a
-                        >
-                    </p>
-                </div>
-            </div>
-        </div>
+    <div class="container px-4 py-16 max-w-screen-sm">
         <div
-            class="hidden sm:flex flex-col justify-center items-center gap-4 text-zinc-200 font-mono h-[100svh]"
+            class="flex flex-col justify-center items-center gap-12 text-zinc-200 font-mono"
         >
-            <h1 class="font-semibold text-3xl">hey there 👋🏻</h1>
-            <p class="mt-8">i haven't built a desktop site quite yet...</p>
+            <h1 class="font-semibold text-3xl">hey there! it's Jeremy 👋🏻</h1>
+            <div class="flex gap-4 items-center">
+                <p>this is me 👉🏻</p>
+                <div
+                    class="w-24 h-24 rounded-full bg-cover bg-center"
+                    style="background-image: url('/img/jeremy-profile.jpg')"
+                ></div>
+            </div>
             <p>
-                but in the meantime, you can follow me
+                I'm a software engineer working at
                 <a
-                    href="https://www.instagram.com/jeremy.garey/"
+                    href="https://mirusresearch.com"
+                    target="_blank"
+                    class="text-orange-300"
+                    >Mirus Research</a
+                >.
+            </p>
+            <div>
+                <p>
+                    I play the drums and sometimes make covers of fun songs.
+                    Here's where you can check them out:
+                </p>
+                <div
+                    class="gap-2 mt-4 text-sm font-sans font-bold grid grid-cols-3"
+                >
+                    <a
+                        href="https://www.youtube.com/@jeremyondrums"
+                        target="_blank"
+                        class="px-2 py-1 bg-red-100 text-red-700 rounded-md flex gap-2 items-center justify-center"
+                    >
+                        <div
+                            class="w-5 h-5 rounded-full bg-cover bg-right"
+                            style="
+                                background-image: url('/img/drum-profile.jpg');
+                            "
+                        ></div>
+                        YouTube
+                    </a>
+                    <a
+                        href="https://www.tiktok.com/@jeremygarey"
+                        target="_blank"
+                        class="px-2 py-1 bg-emerald-100 text-emerald-700 rounded-md flex gap-2 items-center justify-center"
+                    >
+                        <div
+                            class="w-5 h-5 rounded-full bg-cover bg-right"
+                            style="
+                                background-image: url('/img/jeremy-profile.jpg');
+                            "
+                        ></div>
+                        TikTok
+                    </a>
+                    <a
+                        href="https://www.instagram.com/jeremyondrums"
+                        target="_blank"
+                        class="px-2 py-1 bg-fuchsia-100 text-fuchsia-700 rounded-md flex gap-2 items-center justify-center"
+                    >
+                        <div
+                            class="w-5 h-5 rounded-full bg-cover bg-right"
+                            style="
+                                background-image: url('/img/drum-profile.jpg');
+                            "
+                        ></div>
+                        Instagram
+                    </a>
+                </div>
+            </div>
+            <p>
+                I also love
+                <a
+                    href="https://www.bible.com/bible/97/1JN.4.9.MSG"
                     target="_blank"
                     class="text-blue-400 font-semibold"
-                    >on instagram</a
-                >
-            </p>
-            <p>
-                also, check out
+                    >Jesus</a
+                >,
                 <a
-                    href="https://vsco.co/jeremygarey/gallery"
+                    href="https://vsco.co/jeremygarey/media/62b91961fd64e2655d62d344"
                     target="_blank"
                     class="text-blue-400 font-semibold"
-                    >my vsco</a
-                >
+                    >wakeboarding</a
+                >, and
+                <a
+                    href="https://boxedwaterisbetter.com/"
+                    target="_blank"
+                    class="text-blue-400 font-semibold"
+                    >Boxed Water</a
+                >.
             </p>
+            <div>
+                <p>Last thing - here's my IG and VSCO :)</p>
+                <div class="grid grid-cols-2 gap-2 text-sm mt-4">
+                    <a
+                        href="https://www.instagram.com/jeremy.garey"
+                        target="_blank"
+                        class="bg-fuchsia-100 text-fuchsia-700 font-sans py-2 px-4 rounded-xl font-bold flex gap-2 items-center"
+                    >
+                        <div
+                            class="w-8 h-8 rounded-full bg-cover bg-right"
+                            style="
+                                background-image: url('/img/jeremy-profile.jpg');
+                            "
+                        ></div>
+                        @jeremy.garey
+                    </a>
+                    <a
+                        href="https://vsco.co/jeremygarey"
+                        target="_blank"
+                        class="bg-zinc-200 text-zinc-700 font-sans py-2 px-4 rounded-xl font-bold flex gap-2 items-center"
+                    >
+                        <div
+                            class="w-8 h-8 rounded-full bg-cover bg-right"
+                            style="
+                                background-image: url('/img/camera-profile.jpg');
+                            "
+                        ></div>
+                        @jeremy.garey
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </template>
